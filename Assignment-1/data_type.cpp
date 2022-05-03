e09d662dc55276687c4fbcce2bd79cf8cfa270da
Vishnu798 <104858303+Vishnu798@users.noreply.github.com>
2022-05-03 23:28:03 +0530

    Create data_type.cpp

---
+++ Assignment-1/data_type.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+int main(){
+ int n;
+    long n1;
+    char n2;
+    float n3;
+    double n4;
+    cin>>n;
+    cin>>n1;
+    cin>>n2;
+    cin>>n3;
+    cin>>n4;
+    cout<<n<<endl;
+    cout<<n1<<endl;
+    cout<<n2<<endl;
+    printf("%.2f\n", n3);
+    printf("%.9f\n", n4);
+    return 0;
+}
