9aa97c207992b0f9b7c6a1a55f40814dc7751b09
Vishnu798 <104858303+Vishnu798@users.noreply.github.com>
2022-05-04 22:03:29 +0530

    Create two-largest-numbers.cpp

---
+++ Assignment-2/two-largest-numbers.cpp
@@ -0,0 +1,19 @@
+vector<int>getTwoLargestNumbers(vector<int>&A){
+    int max1=INT_MIN, max2=INT_MIN;
+    for(int i=0;i<A.size();i++){
+        if(A[i] > max1){
+            
+            max2=max1;
+            max1=A[i];
+        }
+        else{
+            if(max2<A[i]&&A[i]!=max1){
+                max2=A[i];
+            }
+        }
+    }
+    //write your code to find max1 and max2
+    
+    
+    return {max1,max2};
+}
