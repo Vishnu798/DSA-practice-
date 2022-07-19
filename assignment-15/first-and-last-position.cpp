8cbd31db52444fdb4e63c44dd47b9c7d8f293527
Vishnu798 <104858303+Vishnu798@users.noreply.github.com>
2022-07-20 01:46:38 +0530

    Create first-and-last-position.cpp

---
+++ assignment-15/first-and-last-position.cpp
@@ -0,0 +1,76 @@
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int start =0;
+        bool flag=0;
+        int end = nums.size()-1;
+        sort(nums.begin(), nums.end());
+        vector<int> index;
+        while(start<=end){
+            int mid = (start + end )/2;
+            cout<<"mid is : "<<mid<<"\n";
+            if(nums[mid]==target){
+                    flag=1;              
+                cout<<"mid is now equal to target : "<<mid<<"\n";
+                index.push_back(mid);
+                long l, r;
+                l=mid-1;
+                cout<<"value of l :"<<l<<endl; 
+                r=mid+1;
+                cout<<"value of r :"<<r<<endl; 
+                for(int i=l;i>=0;i--){
+                    cout<<"go in for "<<endl;
+                    if(nums[i]==target){
+                        cout<<"goes in if"<<endl;
+                       // cout<<nu<<endl;
+                        index.push_back(i);
+                    }
+                    else{
+                        break;
+                    }
+                }
+                for(int i=r;i<nums.size();i++){
+                    if(nums[i]==target){
+                        index.push_back(i);
+                    }
+                    else{
+                        break;
+                    }
+                }
+                break;
+             
+            }
+            if(nums[mid] > target){
+                end = mid-1;
+                cout<<"end is : "<<end<<"\n";
+            }
+            if(nums[mid] < target){
+                start = mid + 1;
+                cout<<"start is : "<<start<<"\n";
+            }
+            
+        }
+        sort(index.begin(), index.end());
+         // for(int i=0;i<index.size();i++){
+         //     nums[i] = index[i];
+         // }
+      
+        vector<int> final;
+        int f,last;
+        if(flag==1){
+         f=index[0];
+        cout<<"f is "<<f;
+         last=index[index.size()-1];
+        final.push_back(f);
+         final.push_back(last);
+        return final;
+        }
+        else{
+        f=-1;
+        last=-1;
+           final.push_back(f);
+         final.push_back(last);
+        return final;  
+        }
+    }
+};
