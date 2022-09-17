36099142406bff8fc3a37fc64165c63ab0b80c64
Vishnu798 <104858303+Vishnu798@users.noreply.github.com>
2022-09-17 14:17:44 +0530

    Create intersection-list.cpp

---
+++ assignment-28/intersection-list.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        unordered_set<ListNode*> s;
+        while(headA!=NULL)
+        {
+            s.insert(headA);
+            headA=headA->next;
+        }
+        while(headB!=NULL)
+        {
+            if(s.find(headB)!=s.end())
+                return headB;
+            headB=headB->next;
+        }
+        return NULL;
+        
+    }
+};
+Footer
+© 2022 GitHub, Inc.
