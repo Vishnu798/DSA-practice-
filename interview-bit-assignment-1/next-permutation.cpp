4603e64be78d51407b43d225d923996f5781ce87
Vishnu798 <104858303+Vishnu798@users.noreply.github.com>
2022-09-26 23:07:26 +0530

    Create next-permutation.cpp

---
+++ interview-bit-assignment-1/next-permutation.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+            int n=nums.size(),i=0,l=0;
+        for(i = n-2; i>=0; i--){
+            if(nums[i]<nums[i+1]){
+                break;
+        }
+    }
+        if(i<0){
+            reverse(nums.begin(),nums.end());
+        }
+        else{
+            for(l=n-1;l>i;l--){
+                if(nums[l]>nums[i]){
+                    break;
+                }
+            }
+            swap(nums[i],nums[l]);
+            sort(nums.begin()+i+1,nums.end());
+        }
+    }
+};
